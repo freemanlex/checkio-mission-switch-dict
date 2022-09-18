1971fa1ad0c74367a68eefd00d8f07ae758daf2f
Oleksandr Zozulia <freemanlex@gmail.com>
2022-09-18 14:53:04 +0300

    upd

--- verification/tests.py
+++ verification/tests.py
@@ -12,37 +12,37 @@ TESTS = {
     "Basics": [
         {
             "input": [{"rouses": "red", "car": "red", "sky": "blue"}],
-            "answer": {"red": {"rouses", "car"}, "blue": {"sky"}}
+            "answer": {"red": {"rouses", "car"}, "blue": {"sky"}},
         },
         {
-            "input": [{"1": "1", "2": "2", "3": "3", "4": "4"}],
-            "answer": {"1": {"1"}, "2": {"2"}, "3": {"3"}, "4": {"4"}}
+            "input": [{1: 1, 2: 2, 3: 3, 4: 4}],
+            "answer": {"1": {"1"}, "2": {"2"}, "3": {"3"}, "4": {"4"}},
         }
     ],
     "Extra": [
         {
             "input": [{"a": "b", "b": "c", "c": "a"}],
-            "answer": {"b": {"a"}, "c": {"b"}, "a": {"c"}}
+            "answer": {"b": {"a"}, "c": {"b"}, "a": {"c"}},
         },
         {
             "input": [{"": "", "a": "", "b": "", "c": ""}],
-            "answer": {"": {"", "a", "b", "c"}}
+            "answer": {"": {"", "a", "b", "c"}},
         },
         {
-            "input": [{2: "2", "2": 2}],
-            "answer": {"2": {2}, 2: {"2"}}
+            "input": [{2: 2, "2": 2}],
+            "answer": {"2": {"2"}},
         },
         {
             "input": [{0: "zero"}],
-            "answer": {"zero": {0}}
+            "answer": {"zero": {"0"}},
         },
         {
-            "input": [{2: "number", True: "bool", "text": "string", "2": "string", 100.0: "number"}],
-            "answer": {"number": {2, 100.0}, "bool": {True}, "string": {"2", "text"}}
+            "input": [{2: "number", 1: "bool", "text": "string", "3": "string", 100.0: "number"}],
+            "answer": {"number": {"2", "100.0"}, "bool": {"1"}, "string": {"3", "text"}},
         },
         {
             "input": [{1: "number", 2: "number", 3: "number"}],
-            "answer": {"number": {1, 2, 3}}
+            "answer": {"number": {"1", "2", "3"}},
         }
     ]
 }
